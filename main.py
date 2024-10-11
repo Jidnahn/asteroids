ae8805bf18652903c70a28c0a19295dfb6af5d6b
jidnahn <jidnahn@gmail.com>
2024-10-11 12:09:51 -0500

    60 fps setting

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+        # 60 fps gamers
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 if __name__ == "__main__":
     main()
